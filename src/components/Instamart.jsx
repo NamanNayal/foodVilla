15836e107bf79699ad44911e2fcae6688dc9d42f
Naman Nayal <nayalnaman@gmail.com>
2024-11-20 22:34:52 +0530

    not so cool way of props drilling - lifting the state up

--- src/components/Instamart.jsx
+++ src/components/Instamart.jsx
@@ -1,13 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Instamart() {
+const Section = ({title, description, isVisible, setIsVisible}) =>{
+
+  return (
+  <div className='border border-black p-2 m-2'>
+    <h3 className='font-bold text-xl'>{title}</h3>
+    {
+      isVisible ?( <button onClick={()=>setIsVisible(false)
+      } className='cursor-pointer underline'>Hide
+      </button> )
+      : (
+      <button onClick={()=>setIsVisible(true)} className='cursor-pointer underline'>Show</button>)
+    }
+    {isVisible && <p>{description}</p>}
+  </div>
+  )
+}
+
+const Instamart = () => {
+  const [sectionConfig, setSectionConfig] = useState({
+    showAbout : false,
+    showTeam : false,
+    showCareers :false,
+  })
   return (
     <div>
-        <h1>InstaMart</h1>
-        <h1>100s of components inside it</h1>
+      <h1 className='text-3xl p-2 m-2 font-bold'>InstaMart</h1>
+      <Section 
+      title={"About InstaMart"}
+      description={"This is the about section of InstaMart"}
+      isVisible={sectionConfig.showAbout}
+      setIsVisible={()=>
+        setSectionConfig({
+          showAbout : true,
+          showTeam : false,
+          showCareers :false,
+
+        })
+      }/>
+
+      <Section 
+      title = {"About Team"}
+      description={"This is the about section of Team"}
+      isVisible={sectionConfig.showTeam}
+      setIsVisible={()=>
+        setSectionConfig({
+          showAbout : false,
+          showTeam : true,
+          showCareers :false,
+        })
+      }
+      />
+            <Section 
+      title = {"About Team"}
+      description={"This is the about section of Team"}
+      isVisible={sectionConfig.showCareers}
+      setIsVisible={()=>
+        setSectionConfig({
+          showAbout : false,
+          showTeam : false,
+          showCareers :true,
+        })
+      }
+      />
       
     </div>
   )
 }
 
-export default Instamart
+export default Instamart;
